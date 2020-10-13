9775956829ae1be61eaef8d023452ef9f44ac647
Alonso271 <72246922+Alonso271@users.noreply.github.com>
2020-10-13 09:23:53 +0200

    Create 01.php

---
+++ 01.php
@@ -0,0 +1,10 @@
+<?php
+$num =  random_int(1,10);
+$num1 =  random_int(1,10);
+echo "$num + $num1 = ".          ($num+$num1)."<br/>";
+echo "$num - $num1 = ".         ($num-$num1)."<br/>";
+echo "$num * $num1 = ".($num*$num1)."<br/>";
+echo "$num / $num1 = ".      ($num/$num1)."<br/>";
+echo "$num % $num1 = ".        ($num%$num1)."<br/>";
+echo "$num ** $num1 = ".     ($num**$num1)."<br/>";
+?>
